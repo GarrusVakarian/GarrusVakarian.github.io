96f6821b8741084158a23455c6ae911909e61c6e
Garrus Vakarian <tinkeringturian@gmail.com>
2017-09-09 15:00:43 +0200

    Deleted everything. Project has moved.

--- scenes/minimal/js/afterload.js
+++
@@ -1,9 +0,0 @@
-/**
- * The js/afterload.js file gets called after the scene is fully loaded and
- * displayed. It can be used to trigger fancy on loading effects, such as
- * changing the theme, starting your own animations or for making a dramatic
- * intro. The file itself is mandatory, yet any content within is completely
- * optional.
- */
-
-// autosave("Entered my scene."); // Uncomment this line if you want the game to autosave once your scene has been correctly loaded.
